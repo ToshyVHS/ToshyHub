5860acd4b9b99c766a6b19ec0a24bb0d995d7d43
ToshyVHS <toshyvhs@gmail.com>
2024-10-10 21:48:53 -0300

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,34 @@
+local function safeLoad(url)
+    local success, errorMsg = pcall(function()
+        loadstring(game:HttpGet(url))()
+    end)
+    
+    if success then
+        print("Script Executed Successfull!")
+    else
+        warn("Error Execute The Script: " .. errorMsg)
+    end
+end
+
+local gameId = game.PlaceId
+
+if gameId == 2753915549 or gameId == 4442272183 or gameId == 7449423635 then
+    -- Blox Fruits
+    print("Blox Fruits Detected, Running Script...")
+    local MenuUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZoiIntra/SetUIY/main/One.lua"))()
+MenuUI:WindowCreate("Toshy Hub","rbxassetid://80086118575102","dsc.gg/toshyscript")
+MenuUI:ButtonAdd("Menu",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ToshyVHS/ToshyHub/refs/heads/main/BloxFruits.lua"))()
+    MenuUI:WindowDelete()
+end)
+MenuUI:ButtonAdd("Kaitun",function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/ToshyVHS/ToshyHub/refs/heads/main/KaitunBF.lua"))()
+    MenuUI:WindowDelete()
+end)
+elseif gameId == 142823291 then
+    -- Meme Sea
+    print("Murder Mystery 2 Detected, Running Script")
+    safeLoad("https://raw.githubusercontent.com/ToshyVHS/ToshyHub/refs/heads/main/MurderMystery2.lua")
+else
+    print("Unrecognized Game. No Scripts Were Executed.")
+end
