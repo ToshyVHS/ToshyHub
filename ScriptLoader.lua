48c127be18a03ed9212e8e170449b3e09cd2dd6e
ToshyVHS <toshyvhs@gmail.com>
2024-10-14 09:13:12 -0300

    Update ScriptLoader.lua

--- ScriptLoader.lua
+++ ScriptLoader.lua
@@ -15,16 +15,7 @@ local gameId = game.PlaceId
 if gameId == 2753915549 or gameId == 4442272183 or gameId == 7449423635 then
     -- Blox Fruits
     print("Blox Fruits Detected, Running Script...")
-    local MenuUI = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZoiIntra/SetUIY/main/One.lua"))()
-MenuUI:WindowCreate("Toshy Hub","rbxassetid://80086118575102","dsc.gg/toshyscript")
-MenuUI:ButtonAdd("Menu",function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ToshyVHS/ToshyHub/refs/heads/main/MainBF.lua"))()
-    MenuUI:WindowDelete()
-end)
-MenuUI:ButtonAdd("Kaitun",function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ToshyVHS/ToshyHub/refs/heads/main/KaitunBF.lua"))()
-    MenuUI:WindowDelete()
-end)
+    safeLoad("https://raw.githubusercontent.com/ToshyVHS/ToshyHub/refs/heads/main/MainBF.lua")
 elseif gameId == 142823291 then
     -- Murder Mystery 2
     print("Murder Mystery 2 Detected, Running Script")
