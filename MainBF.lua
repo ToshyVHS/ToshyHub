d237031b32302db4d106f9788cddc7c2275aeafa
ToshyVHS <toshyvhs@gmail.com>
2024-10-19 21:12:29 -0300

    Update MainBF.lua

--- MainBF.lua
+++ MainBF.lua
@@ -1,1 +1,2 @@
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/ToshyVHS/OBF/refs/heads/main/ToshyMainBF.lua"))()
+  setclipboard("https://discord.gg/m3MQHYz3K5")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToshyVHS/OBF/refs/heads/main/ToshyMainBF.lua"))()
