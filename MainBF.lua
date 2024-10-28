d75d6f5f54bece09a12a2e1cb2db1d105cc7145e
ToshyVHS <toshyvhs@gmail.com>
2024-10-28 20:02:25 -0300

    Update MainBF.lua

--- MainBF.lua
+++ MainBF.lua
@@ -1,2 +1,7 @@
   setclipboard("https://discord.gg/m3MQHYz3K5")
 loadstring(game:HttpGet("https://raw.githubusercontent.com/ToshyVHS/OBF/refs/heads/main/ToshyMainBF.lua"))()
+  game.StarterGui:SetCore("SendNotification",{
+		Title = "Toshy Hub";
+		Text = "Discord Copied To Clipboard";
+		Duration = 7;
+	})
