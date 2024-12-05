37ae157809377b847b30a963b6b63b87ebea32a6
ToshyVHS <toshyvhs@gmail.com>
2024-12-05 01:08:41 -0300

    Update MainMM2.lua

--- MainMM2.lua
+++ MainMM2.lua
@@ -1,1 +1,1 @@
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/ToshyVHS/OBF/refs/heads/main/TsMainMM2.lua"))()
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/ToshyWare/Obfuscated/refs/heads/main/TsMM2.lua"))()
