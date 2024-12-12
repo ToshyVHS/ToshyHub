79548b2b432d0012519a784a062a048886411072
ToshyVHS <toshyvhs@gmail.com>
2024-12-12 00:24:10 -0300

    Update MainMemeSea.lua

--- MainMemeSea.lua
+++ MainMemeSea.lua
@@ -1,1 +1,1 @@
-game.Players.LocalPlayer:Kick("Discontinued, Join Discord to Get the New Script")
+game.Players.LocalPlayer:Kick("Discontinued, Join Discord to Get the New Script, discord.gg/m3MQHYz3K5")
